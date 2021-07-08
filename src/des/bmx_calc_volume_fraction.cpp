95bc499ddf9891cb6293ac5bf021c64d9a76e8db
Ann Almgren <asalmgren@lbl.gov>
2021-07-07 17:17:56 -0700

    Update bmx_calc_volume_fraction to use AMReX-native multilevel ParticleToMesh routine rather than doing it ourselves -- this now corrrectly handles any deposition that might occur across coarse-fine boundaries

--- src/des/bmx_calc_volume_fraction.cpp
+++ src/des/bmx_calc_volume_fraction.cpp
@@ -1,28 +1,62 @@
 #include <bmx.H>
 #include <bmx_pc.H>
+#include <bmx_chem.H>
+#include <bmx_deposition_K.H>
+#include <AMReX_AmrParticles.H>
 
 // This re-calculates the volume fraction within the domain
 // but does not change the values outside the domain
-void bmx::bmx_calc_volume_fraction (int lev, MultiFab& vf)
+void bmx::bmx_calc_volume_fraction ()
 {
-    const Geometry& gm  = Geom(lev);
+  int start_part_comp = realIdx::vol;
+  int start_mesh_comp = 0;
+  int        num_comp = 1;
 
-    // Deposit particle volume to the grid
-    pc->SolidsVolumeDeposition(lev, vf);
+  if (bmx::m_deposition_scheme == DepositionScheme::trilinear) 
+  {
+      // Initialize to zero because the deposition routine will only change values where there
+      // are particles
+      Real zero_out_input = true;
 
-    // Move any volume deposited outside the domain back into the domain
-    // when BC is either a pressure inlet or mass inflow.
-    bmx_deposition_bcs(lev, vf);
+      ParticleToMesh(*pc,get_vf(),0,finest_level,
+                     TrilinearDeposition_new{start_part_comp,start_mesh_comp,num_comp},
+                     zero_out_input);
+#if 0
+  } else if (bmx::m_deposition_scheme == DepositionScheme::square_dpvm) {
 
-    // Sum grid boundaries to capture any material that was deposited into
-    // your grid from an adjacent grid.
-    vf.SumBoundary(gm.periodicity());
+    ParticleToMesh(*pc,get_vf(),0,finest_level,DPVMSquareDeposition());
+
+  } else if (bmx::m_deposition_scheme == DepositionScheme::true_dpvm) {
+
+    ParticleToMesh(*pc,get_vf(),0,finest_level,TrueDPVMDeposition());
+
+  } else if (bmx::m_deposition_scheme == DepositionScheme::centroid) {
+
+    ParticleToMesh(*pc,get_vf(),0,finest_level,CentroidDeposition());
+#endif
+
+  } else {
+    amrex::Abort("Don't know this deposition_scheme!");
+  }
+
+  for (int lev = 0; lev <= finest_level; lev++)  
+  {
+    MultiFab& vf = *get_vf()[lev];
+    Geometry& gm = Geom(lev); 
 
     // Now define this mf = (1 - particle_vol)
     vf.mult(-1.0, vf.nGrow());
     vf.plus( 1.0, vf.nGrow());
 
+    // Move any volume deposited outside the domain back into the domain
+    // when BC is either a pressure inlet or mass inflow.
+    bmx_deposition_bcs(lev, vf);
+
     // Fill the boundaries so we calculate the correct average
     // solids volume fraction for periodic boundaries.
     vf.FillBoundary(gm.periodicity());
+
+    amrex::Print() << "AFTER" << std::endl;
+    print_state(vf,IntVect(7,7,11));
+  }
 }
