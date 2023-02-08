fdbe83d16e6115f4f5d8320f1ce10eb4444fdac0
Bruce J Palmer <d3g293@constance01.pnl.gov>
2023-02-08 07:42:33 -0800

    Committing current status for debugging purposes.

--- src/io/bmx_plt.cpp
+++ src/io/bmx_plt.cpp
@@ -243,12 +243,12 @@ bmx::WritePlotFile (std::string& plot_file, int nstep, Real time )
         int_comp_names.push_back("cell_type");
         int_comp_names.push_back("number_bonds");
         int_comp_names.push_back("unique_id_1");
-        int_comp_names.push_back("home_proc_1");
         int_comp_names.push_back("unique_id_2");
-        int_comp_names.push_back("home_proc_2");
         int_comp_names.push_back("unique_id_3");
-        int_comp_names.push_back("home_proc_3");
         int_comp_names.push_back("unique_id_4");
+        int_comp_names.push_back("home_proc_1");
+        int_comp_names.push_back("home_proc_2");
+        int_comp_names.push_back("home_proc_3");
         int_comp_names.push_back("home_proc_4");
         int_comp_names.push_back("site_1");
         int_comp_names.push_back("site_2");
@@ -260,6 +260,12 @@ bmx::WritePlotFile (std::string& plot_file, int nstep, Real time )
         int_comp_names.push_back("fusion_new_flag");
         int_comp_names.push_back("fusion_id");
         int_comp_names.push_back("fusion_cpu");
+        int_comp_names.push_back("deleted_site_1");
+        int_comp_names.push_back("deleted_site_2");
+        int_comp_names.push_back("deleted_id_1");
+        int_comp_names.push_back("deleted_id_2");
+        int_comp_names.push_back("deleted_cpu_1");
+        int_comp_names.push_back("deleted_cpu_2");
         int_comp_names.push_back("unique_id");
         int_comp_names.push_back("home_cpu");
         for (i=intIdx::count-1; i<MAX_CHEM_INT_VAR; i++) {
